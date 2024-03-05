f5ec34856128dc3cfc8804fe160cfba21c2616b4
JohnChristianMaliwat <maliwat.johnchristian.soleta@gmail.com>
2024-03-05 16:25:59 +0800

    My Commit

---
+++ register.js
@@ -0,0 +1,29 @@
+console.log("Hello World!")
+console.error("ERROR HAPPENED!")
+console.warn("WARNINNGGG!!!!")
+
+// Variables
+// string = "Wordsss"
+// number = 123
+// boolean = true or false
+// object = {} () []
+
+// typeof
+console.log(typeof(123))
+
+let name = "JC"
+name = 10;
+console.log(name)
+
+let age = 11; // number
+let me = "John" // string
+let isProf= false // boolean
+hobbies = ["hello", "hi", "nice"] //object
+run = function () {
+    console.log("ETO NAA!")
+};// function
+console.log(age, me, isProf, run);
+console.log(typeof run);
+console.table(hobbies)
+console.log("eto ang akin pangalan" + me );
+console.log(`eto ang akin pangalan ${me}`);
